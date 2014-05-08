487a2ed7eb3b565e20de29ca3a871bf8d686ff3a
Sian Cao <siyuan.cao@i-soft.com.cn>
2014-05-08 15:49:37 +0800

    modify atlas effects

--- atlas_frag.glsl
+++ atlas_frag.glsl
@@ -9,23 +9,12 @@ varying vec2 texcoord;
 
 #define PI 3.1415926
 
-vec4 effects(){
-    vec2 p = (gl_FragCoord.xy - 0.5 * resolution.xy) / min(resolution.x, resolution.y);
-    vec2 t = vec2(gl_FragCoord.xy / resolution.xy);
-    
-    vec3 c = vec3(0);
-    
-    for(int i = 0; i < 10; i++) {
-            float t = 0.4 * PI * float(i) / 30.0 * time * 0.5;
-            float x = cos(3.0*t);
-            float y = sin(4.0*t);
-            vec2 o = 0.2 * vec2(x, y);
-            float r = fract(x);
-            float g = 0.5 - r;
-            c += 0.02 / (length(p-o)) * vec3(r, g, 0.9);
-        }
-    
-    return vec4(c, 1);
+vec4 effects() {
+    vec2 p = ( gl_FragCoord.xy / resolution.xy ) - (cos(time) + 2.0)/5.;
+    float sx = 0.3 * (p.x + 0.8) * sin( 5.0 * p.x - 2. * time);
+    float dy = 4./ ( 30. * abs(p.y - sx));
+    dy += 1./ (60. * length(p - vec2(p.x, 0.)));
+    return vec4( (p.x + 0.4) * dy, 0.3 * dy, dy, 1.0 );
 }
 
 void main()
